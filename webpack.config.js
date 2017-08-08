8e19c003e27d7dee4b703ca1325f940798264166
yunweb <3358621@qq.com>
2017-08-08 23:02:46 +0800

    初始化

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+var webpack = require("webpack");
+var path = require("path");
+module.exprots = {
+    entry: "./index.js",
+    output: {
+        path: "./build",
+        filename: "bundle.js"
+    }
+}
