6f41df21d7d0c10961479813aff789416c1d30a5
yunweb <3358621@qq.com>
2017-08-10 11:04:18 +0800

    test

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import _ from "lodash";
+import styles from "./src/css/main.css";
 
 function component() {
     var element = document.createElement("div");
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello', 'World!'], ' ');
     return element;
 }
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,23 @@
-var webpack = require("webpack");
-var path = require("path");
-module.exprots = {
-    entry: "./index.js",
+module.exports = {
+    devtool: 'eval-source-map', //配置生成source maps，如果是生产环境，请换为source-map
+    entry: __dirname + "/index.js", //已多次提及的唯一入口文件
     output: {
-        path: "./build",
-        filename: "bundle.js"
+        path: __dirname + "/build", //打包后的文件存放的地方
+        filename: "bundle.js" //打包后输出文件的文件名
+    },
+    module: {
+        loaders: [{
+            test: /\.css$/,
+            loader: 'style!css?modules', //添加对样式表的处理
+            include: './src/css',
+            exclude: './node_modules'
+        }]
+    },
+    devServer: {
+        contentBase: __dirname, //本地服务器所加载的页面所有目录
+        port: 8080, //默认监听端口，8080
+        colors: true, //终端输出结果为彩色
+        historyApiFallback: true, //不跳转
+        inline: true //实时刷新
     }
 }
